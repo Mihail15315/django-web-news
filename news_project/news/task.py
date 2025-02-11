85bfa578fdbba0bcc677eca3d1c4f285f6d4fd83
Mihail15315 <106290633+Mihail15315@users.noreply.github.com>
2025-02-11 17:34:36 +0700

    Update task.py

--- news_project/news/task.py
+++ news_project/news/task.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 # from .models import News
 from constance import config
 from django.apps import apps
+import os
+email_host_user = os.getenv('EMAIL_HOST_USER')
 @shared_task(name='news.send_email_task')
 def send_email_task():
     News = apps.get_model('news', 'News')
@@ -18,4 +20,4 @@ def send_email_task():
         subject = config.EMAIL_SUBJECT
         body = config.EMAIL_BODY + '\n\n' + '\n'.join([f"{news.title}: {news.text}" for news in news_today])
 
-        send_mail(subject, body, 'mihailbaranov16072002@gmail.com', recipients)
+        send_mail(subject, body, email_host_user, recipients)
